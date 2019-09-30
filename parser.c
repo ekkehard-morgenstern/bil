42c463c5d6fa7155edbf0ce6ed59cf972892300a
Ekkehard Morgenstern <ekkehard@ekkehardmorgenstern.de>
2019-09-30 02:23:01 +0200

    parser

--- parser.c
+++ parser.c
@@ -53,16 +53,16 @@ static char* xstrdup( const char* text ) {
 static void dump_tree_node( treenode_t* node, int indent ) {
     if ( node == 0 ) return;
     if ( node->text == 0 ) {
-        printf( "%-*.*s%s\n", indent, indent, "", token2text(node->nodetype) );
+        printf( "%-*.*s%d\n", indent, indent, "", (int) node->nodetype );
     } else {
-        printf( "%-*.*s%s '%s'\n", indent, indent, "", token2text(node->nodetype), node->text );
+        printf( "%-*.*s%d '%s'\n", indent, indent, "", (int) node->nodetype, node->text );
     }
     for ( size_t i=0; i < node->numBranches; ++i ) {
         dump_tree_node( node->branches[i], indent+2 );
     }
 }
 
-static treenode_t* create_node( treenode_t nodetype, const char* text ) {
+static treenode_t* create_node( nodetype_t nodetype, const char* text ) {
     treenode_t* node = (treenode_t*) xmalloc( sizeof(treenode_t) );
     node->nodetype     = nodetype;
     node->text         = text ? xstrdup(text) : 0;
@@ -92,26 +92,83 @@ static void add_branch( treenode_t* node, treenode_t* branch ) {
     node->branches[ node->numBranches++ ] = branch;
 }
 
-static parsingnode_t* find_parsing_node( nodetype_t nodetype ) {
+static const parsingnode_t* find_parsing_node( nodetype_t nodetype ) {
     size_t numParsingNodes = sizeof(parsingTable) / sizeof(parsingnode_t);
     for ( size_t i=0; i < numParsingNodes; ++i ) {
-        parsingnode_t* node = &parsingTable[i];
+        const parsingnode_t* node = &parsingTable[i];
         if ( node->nodeType == nodetype ) return node;
     }
     return 0;
 }
 
-treenode_t* parse_node( const char** pPos, parsingnode_t* node ) {
+static void fatal( const char* fmt, ... ) {
+    va_list ap;
+    va_start( ap, fmt );
+    fprintf( stderr, "fatal: " );
+    vfprintf( stderr, fmt, ap );
+    fprintf( stderr, "\n" );
+    va_end( ap );
+    exit( EXIT_FAILURE );
+}
 
-
+static treenode_t* parse_node( const parsingnode_t* node, const char** pTextPos ) {
+    treenode_t* temp; treenode_t* result = 0; size_t i;
+    switch ( node->nodeClass ) {
+        case NC_TERMINAL:
+            switch ( node->termType ) {
+                case TT_STRING:
+                    break;
+                case TT_REGEX:
+                    break;
+                default:
+                    fatal( "bad parsing node (type A)" );
+            }
+            break;
+        case NC_PRODUCTION:
+            if ( node->numBranches != 1U ) fatal( "bad parsing node (type C)" );
+            return parse_node( &parsingTable[branches[node->branches]], pTextPos );           
+        case NC_MANDATORY:
+            result = create_node( node->nodeType, 0 );
+            for ( i=0; i < node->numBranches; ++i ) {
+                temp = parse_node( &parsingTable[branches[node->branches+i]], pTextPos );
+                if ( temp == 0 ) { delete_node( result ); return 0; }
+                add_branch( result, temp );
+            }
+            break;
+        case NC_ALTERNATIVE:
+            for ( i=0; i < node->numBranches; ++i ) {
+                temp = parse_node( &parsingTable[branches[node->branches+i]], pTextPos );
+                if ( temp ) return temp;
+            }
+            return 0;          
+        case NC_OPTIONAL:
+            if ( node->numBranches != 1U ) fatal( "bad parsing node (type D)" );
+            return parse_node( &parsingTable[branches[node->branches]], pTextPos );
+        case NC_OPTIONAL_REPETITIVE:
+            if ( node->numBranches != 1U ) fatal( "bad parsing node (type E)" );
+            result = create_node( node->nodeType, 0 );
+            for (;;) {
+                temp = parse_node( &parsingTable[branches[node->branches]], pTextPos );
+                if ( temp == 0 ) break;
+                add_branch( result, temp );
+            }
+            break;
+        default:
+            fatal( "bad parsing node (type B)" );
+    }
+    if ( result ) {
+        if ( result->numBranches == 0U ) { delete_node( result ); return 0; }
+        if ( result->numBranches == 1U ) {
+            temp = result->branches[0]; result->branches[0] = 0;
+            delete_node( result ); result = temp;
+        }
+    }
+    return result;
 }
 
 treenode_t* parse( const char* text ) {
-    parsingnode_t* root = find_parsing_node( NT_GLOBAL_SCOPE );
-    if ( root == 0 ) return 0;
-    const char* pos = text;
-    parsingnode_t* result = parse_node( &pos, root );
-    if ( result == 0 ) return 0;
-
-    return result;
+    const parsingnode_t* node  = find_parsing_node( NT_GLOBAL_SCOPE );
+    if ( node == 0 ) return 0;
+    const char* textPos = text;
+    return parse_node( node, &textPos );
 }
